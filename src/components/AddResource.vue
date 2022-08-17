92bba0a791971762452e65bb59ea0595bed174c2
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2022-08-17 03:12:09 +0200

    built AddResource component

---
+++ src/components/AddResource.vue
@@ -0,0 +1,35 @@
+<template>
+  <h2>Add resource</h2>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
