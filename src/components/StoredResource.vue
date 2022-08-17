ce4655b8b3534ce9d6597a42c61beed33b73f6d2
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2022-08-17 03:12:59 +0200

    built StoredResource component

---
+++ src/components/StoredResource.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <list-item
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :resource="resource"
+      @delete-resource="deleteResource"
+    ></list-item>
+  </ul>
+</template>
+
+<script>
+import ListItem from './ListItem.vue';
+export default {
+  components: {
+    ListItem,
+  },
+  inject: ['storedResources'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
